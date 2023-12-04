2d8125f32f1038fcee554ffd316f8657828c15b7
YCC Yamamoto <ryo.yamamoto@exc.tokai-rika.co.jp>
2023-12-04 14:03:07 +0900

    init

---
+++ src/DotNetRepositoryTemplate.Library/FileHash.cs
@@ -0,0 +1,82 @@
+﻿#if !NETSTANDARD2_0
+using System.Buffers;
+using System.Globalization;
+#endif
+using System;
+using System.IO;
+using System.Security.Cryptography;
+#if NETSTANDARD2_0
+using System.Text;
+#endif
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetRepositoryTemplate.Library;
+
+public static class FileHash
+{
+    public static async ValueTask<string> GetHashCode(string filePath, CancellationToken cancellationToken)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"ファイルが存在しません。", nameof(filePath));
+        }
+#if NETSTANDARD2_0
+        byte[] buffer;
+
+        using (var fileStream = File.OpenRead(filePath))
+        {
+            buffer = new byte[fileStream.Length];
+
+            _ = await fileStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false);
+        }
+#else
+        var buffer = await File.ReadAllBytesAsync(filePath, cancellationToken).ConfigureAwait(false);
+#endif
+
+        return GetHashCode(buffer);
+    }
+
+    private static string GetHashCode(byte[] bytes)
+    {
+#if NETSTANDARD2_0
+        using var sha256 = SHA256.Create();
+        var hash = sha256.ComputeHash(bytes);
+        var stringBuilder = new StringBuilder();
+
+        foreach (var b in hash)
+        {
+            stringBuilder.Append(b.ToString("x2"));
+        }
+
+        return stringBuilder.ToString();
+#else
+        var hash = SHA256.HashData(bytes);
+        var hexBuffer = ArrayPool<char>.Shared.Rent(hash.Length * 2);
+        var charBuffer = ArrayPool<char>.Shared.Rent(2);
+
+        try
+        {
+            var hexWritten = 0;
+
+            for (var index = 0; index < hash.Length; index++)
+            {
+                hash[index].TryFormat(charBuffer, out var charsWritten, "x2", CultureInfo.InvariantCulture);
+
+                for (var charIndex = 0; charIndex < charsWritten; charIndex++)
+                {
+                    hexBuffer[hexWritten] = charBuffer[charIndex];
+                    hexWritten++;
+                }
+            }
+
+            return hexBuffer[..hexWritten].AsSpan().ToString();
+        }
+        finally
+        {
+            ArrayPool<char>.Shared.Return(hexBuffer);
+            ArrayPool<char>.Shared.Return(charBuffer);
+        }
+#endif
+    }
+}
---
+++ src/DotNetRepositoryTemplate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using DotNetRepositoryTemplate.Library;
+
+if (args.Length != 1)
+{
+    await Console.Error.WriteLineAsync("引数としてファイルパスをひとつ指定してください。").ConfigureAwait(false);
+    return 1;
+}
+
+using var cancellationTokenSource = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, _) =>
+{
+    if (cancellationTokenSource?.IsCancellationRequested is not false)
+    {
+        return;
+    }
+
+    cancellationTokenSource.Cancel();
+};
+Console.WriteLine($"Runtime: {RuntimeInformation.FrameworkDescription}");
+
+var filePath = args[0];
+var hashCode = await FileHash.GetHashCode(filePath, cancellationTokenSource.Token).ConfigureAwait(false);
+
+Console.WriteLine(hashCode);
+return 0;
